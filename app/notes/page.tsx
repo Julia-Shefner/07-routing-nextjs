b8cf2ebe745a15d89c9a9343002e72aff83e482f
Yuliia Shefner <yul.she91@gmail.com>
2026-04-14 16:05:39 +0200

    Initial commit

---
+++ app/notes/page.tsx
@@ -0,0 +1,33 @@
+import {
+  QueryClient,
+  HydrationBoundary,
+  dehydrate,
+} from "@tanstack/react-query";
+import { fetchNotes } from "@/lib/api";
+import NotesClient from "./Notes.client";
+
+type NotesProps = {
+  searchParams: {
+    page?: string;
+    search?: string;
+  };
+};
+
+const Notes = async ({ searchParams }: NotesProps) => {
+  const page = Number(searchParams.page) || 1;
+  const search = searchParams.search ?? "";
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["notes", page, search],
+    queryFn: () => fetchNotes({ page, search }),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NotesClient />
+    </HydrationBoundary>
+  );
+};
+
+export default Notes;
