b5d53d9c5cd0a94c7c09a0d37934756bc285d76c
Yuliia Shefner <yul.she91@gmail.com>
2026-04-15 09:12:36 +0200

    changed routes

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -1,5 +1,5 @@
 import { fetchNotes } from "@/lib/api";
-import NotesClient from "../../Notes.client";
+import NotesClient from "./Notes.client";
 
 type NotesByTagProps = {
   params: Promise<{ slug: string[] }>;
