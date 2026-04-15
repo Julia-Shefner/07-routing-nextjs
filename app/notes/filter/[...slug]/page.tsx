1f324e86df3cca1f5f095a5b43c9edfb948f7409
Yuliia Shefner <yul.she91@gmail.com>
2026-04-15 09:28:17 +0200

    fixed routes

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -13,3 +13,37 @@ const NotesByTag = async ({ params }: NotesByTagProps) => {
 };
 
 export default NotesByTag;
+
+// import {
+//   QueryClient,
+//   HydrationBoundary,
+//   dehydrate,
+// } from "@tanstack/react-query";
+// import { fetchNotes } from "@/lib/api";
+// import NotesClient from "./Notes.client";
+
+// type NotesProps = {
+//   searchParams: {
+//     page?: string;
+//     search?: string;
+//   };
+// };
+
+// const Notes = async ({ searchParams }: NotesProps) => {
+//   const page = Number(searchParams.page) || 1;
+//   const search = searchParams.search ?? "";
+//   const queryClient = new QueryClient();
+
+//   await queryClient.prefetchQuery({
+//     queryKey: ["notes", page, search],
+//     queryFn: () => fetchNotes({ page, search }),
+//   });
+
+//   return (
+//     <HydrationBoundary state={dehydrate(queryClient)}>
+//       <NotesClient />
+//     </HydrationBoundary>
+//   );
+// };
+
+// export default Notes;
